d6c06c76870b092e91b672f36bad0607c80716d0
unknown <rinneeeeeeeeee@gmail.com>
2019-03-13 19:20:40 +0800

    fixed server

--- microservices/barcode/server.py
+++ microservices/barcode/server.py
@@ -1,27 +1,31 @@
 import socket
 #from src.api.reservation import Reservations, Reservation
 
-print('Initializing barcode receiver...')
+def server_init():
+    print('Initializing barcode receiver...')
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 6543        # Port to listen on (non-privileged ports are > 1023)
-done = False
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 6543        # Port to listen on (non-privileged ports are > 1023)
+    done = False
 
-#db_resrs = Reservations()
-#db_resr = Reservation()
+    #db_resrs = Reservations()
+    #db_resr = Reservation()
 
-print('Initialized some stuff...')
+    print('Initialized some stuff...')
 
-while done == False:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(data.decode('utf-8'))
-                conn.sendall(data)
+    while done == False:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(data.decode('utf-8'))
+                    conn.sendall(data)
+
+if __name__ == '__main__':
+    server_init()
